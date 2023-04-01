6d495e8388d2252512ad192ad83e6fbb4a1ea54e
zygvlogs <nylesmanriquez@gmail.com>
2023-04-01 18:25:36 +0300

    changed style added load button

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
-const searchForm = document.querySelector("#search-form");
-const searchInput = document.querySelector("#search-input");
-const resultsDiv = document.querySelector("#results");
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-input');
+const resultsDiv = document.querySelector('#results');
+const loadMoreBtn = document.querySelector('#load-more-btn');
+let currentPage = 0;
+let currentQuery = '';
 
-searchForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const searchTerm = searchInput.value.trim();
-  searchForRecipe(searchTerm);
-});
-
-async function searchForRecipe(searchTerm) {
-  const appId = "Your_App_ID";
-  const appKey = "Your_app_Key";
-  const response = await fetch(`https://api.edamam.com/search?q=${searchTerm}&app_id=${appId}&app_key=${appKey}&from=0&to=20`);
+// Fetch recipes from Edamam API
+async function fetchRecipes(query, page) {
+  const appID = 'Your_APP_ID';
+  const appKey = 'Your_App_KEY';
+  const from = page * 12 + 1;
+  const to = from + 11;
+  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${appID}&app_key=${appKey}&from=${from}&to=${to}`);
   const data = await response.json();
-  displayResults(data.hits);
+  return data;
 }
 
-function displayResults(results) {
-  resultsDiv.innerHTML = "";
-  results.forEach((result) => {
-    const recipeDiv = document.createElement("div");
-    recipeDiv.classList.add("recipe");
-    recipeDiv.innerHTML = `
-      <h2>${result.recipe.label}</h2>
-      <img src="${result.recipe.image}">
-      <p>Calories: ${result.recipe.calories.toFixed(2)}</p>
-      <a href="${result.recipe.url}" target="_blank">View Recipe</a>
-    `;
+// Render recipes in the DOM
+function renderRecipes(recipes) {
+  recipes.hits.forEach(recipe => {
+    const recipeDiv = document.createElement('div');
+    recipeDiv.classList.add('recipe');
+
+    const img = document.createElement('img');
+    img.src = recipe.recipe.image;
+    recipeDiv.appendChild(img);
+
+    const title = document.createElement('h2');
+    title.textContent = recipe.recipe.label;
+    recipeDiv.appendChild(title);
+
+    const calories = document.createElement('p');
+    calories.innerHTML = `Calories: ${Math.round(recipe.recipe.calories)} kcal`;
+    recipeDiv.appendChild(calories);
+
+    const link = document.createElement('a');
+    link.href = recipe.recipe.url;
+    link.target = '_blank';
+    link.textContent = 'View Recipe';
+    recipeDiv.appendChild(link);
+
     resultsDiv.appendChild(recipeDiv);
   });
 }
+
+// Handle search form submission
+searchForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  currentPage = 0;
+  currentQuery = searchInput.value.trim();
+  resultsDiv.innerHTML = '';
+  const data = await fetchRecipes(currentQuery, currentPage);
+  renderRecipes(data);
+  if (data.more) {
+    loadMoreBtn.style.display = 'block';
+  } else {
+    loadMoreBtn.style.display = 'none';
+  }
+});
+
+// Handle load more button click
+loadMoreBtn.addEventListener('click', async () => {
+  currentPage++;
+  const data = await fetchRecipes(currentQuery, currentPage);
+  renderRecipes(data);
+  if (!data.more) {
+    loadMoreBtn.style.display = 'none';
+  }
+});
